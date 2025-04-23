d9d0d7d13142590995f04af5dc9f9cbca8bf0a62
Moe-Wai-Yan <moorewaiyan@gmail.com>
2025-04-23 22:00:58 +0630

    add/subject

---
+++ app/Http/Controllers/Api/SubjectController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Subject;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\SubjectResource;
+
+class SubjectController extends Controller
+{
+    public function index(){
+        $subjects=Subject::all();
+        return response()->json([
+            'message'=>'Subjects retrieved successfully',
+            'data'=>SubjectResource::collection($subjects)
+        ]);
+    }
+
+    public function show($id){
+        $subject=Subject::findOrFail($id);
+        return response()->json([
+            'message'=>'Subject show successfully',
+            'data'=>SubjectResource::make($subject)
+        ]);
+    }
+}
