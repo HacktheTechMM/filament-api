0665a57dabf469d7b7fea293073c158253be502f
Moe-Wai-Yan <moorewaiyan@gmail.com>
2025-04-23 13:08:00 +0630

    add-mentor_subject

--- app/Models/MentorProfile.php
+++ app/Models/MentorProfile.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class MentorProfile extends Model
 {
     protected $guarded = [''];
+
+    public function subjects(){
+        return $this->belongsToMany(MentorProfile::class, 'mentor_subjects', 'mentor_id', 'subject_id');
+    }
 }
